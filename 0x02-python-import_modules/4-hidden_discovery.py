61cac3ebeac918a730457d64b01a6f6c02e00136
Sajith Abubakar <Sajith Abubakar>
2023-10-05 19:00:29 -0700

    Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
+import hidden_4
+
 if __name__ == "__main__":
-    from hidden_4 import *
+    for content in dir(hidden_4):
+        if content[:2] != "__":
+            print("{:s}".format(content))
