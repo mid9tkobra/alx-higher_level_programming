44a6d33e0163413cfa583bc9f9e2eded2a63cbac
Sajith Abubakar <Sajith Abubakar>
2023-10-08 13:45:14 -0700

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-
-from calculator_1 import add, sub, mul, div
-from sys import argv
-
 if __name__ == "__main__":
-        length = len(argv)
-            if length != 4:
-                        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-                                exit(1)
-                                    if length == 4:
-                                                operator = argv[2]
-                                                        a = int(argv[1])
-                                                                b = int(argv[3])
-                                                                        if operator == "+":
-                                                                                        print("{:d} + {:d} = {:d}".forma
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    args_len = len(sys.argv)
+    if args_len != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    opr = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if opr == '+':
+        print("{} {} {} = {}".format(a, opr, b, add(a, b)))
+    elif opr == '-':
+        print("{} {} {} = {}".format(a, opr, b, sub(a, b)))
+    elif opr == '*':
+        print("{} {} {} = {}".format(a, opr, b, mul(a, b)))                                                 elif opr == '/':                                     
+        print("{} {} {} = {}".format(a, opr, b, div(a, b)))                                                 else:                                                                                                       print("Unknown operator. Available operators: +, -, * and /")                                           exit(1)
